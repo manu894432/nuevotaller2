66e83c887cfb0620d7a7dcda5ec6a026f98f819a
manu894432 <divariom888@gmail.com>
2025-08-08 01:35:01 -0500

    crear algoritmo para poner nombre y edad

---
+++ entradaysalida/entradaysalida.py
@@ -0,0 +1,10 @@
+# entrada.py
+# Programa que pide el nombre y edad, luego imprime un mensaje
+
+# Pedir datos al usuario
+nombre = input("Ingrese su nombre: ")
+edad = input("Ingrese su edad: ")
+
+# Mostrar mensaje
+print("Hola", nombre + ", tienes", edad, "años")
+
